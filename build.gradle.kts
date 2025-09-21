0ceb190e043c823b04d40c2a7b161793d6c6278d
skobow <skobow@users.noreply.github.com>
2025-09-21 14:42:55 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    alias(libs.plugins.hivemq.extension)
+    alias(libs.plugins.defaults)
+    alias(libs.plugins.license)
+}
+
+group = "com.hivemq.extensions"
+description = "HiveMQ 4 Hello World Extension - a simple reference for all extension developers"
+
+hivemqExtension {
+    name = "Hello World Extension"
+    author = "HiveMQ"
+    priority = 1000
+    startPriority = 1000
+    mainClass = "$group.helloworld.HelloWorldMain"
+    sdkVersion = "$version"
+
+    resources {
+        from("LICENSE")
+    }
+}
+
+@Suppress("UnstableApiUsage")
+testing {
+    suites {
+        withType<JvmTestSuite> {
+            useJUnitJupiter(libs.versions.junit.jupiter)
+        }
+        "test"(JvmTestSuite::class) {
+            dependencies {
+                implementation(libs.mockito)
+            }
+        }
+        "integrationTest"(JvmTestSuite::class) {
+            dependencies {
+                compileOnly(libs.jetbrains.annotations)
+                implementation(libs.hivemq.mqttClient)
+                implementation(libs.testcontainers.junitJupiter)
+                implementation(libs.testcontainers.hivemq)
+                runtimeOnly(libs.logback.classic)
+            }
+        }
+    }
+}
+
+license {
+    header = rootDir.resolve("HEADER")
+    mapping("java", "SLASHSTAR_STYLE")
+}
+
+/* ******************** debugging ******************** */
+
+tasks.prepareHivemqHome {
+    hivemqHomeDirectory = file("/your/path/to/hivemq-<VERSION>")
+}
+
+tasks.runHivemqWithExtension {
+    debugOptions {
+        enabled = false
+    }
+}
